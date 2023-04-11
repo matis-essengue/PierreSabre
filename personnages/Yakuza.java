1dad9f988fc363697e723ba9a290d1820073cd10
Matis Essengue <85627723+matis-essengue@users.noreply.github.com>
2023-04-11 18:37:14 +0200

    le clan des Yakuzas

--- personnages/Yakuza.java
+++ personnages/Yakuza.java
@@ -9,6 +9,12 @@ public class Yakuza extends Humain {
         this.clan = clan;
     }
 
+    @Override
+    public void direBonjour() {
+        super.direBonjour();
+        parler("Mon clan est celui de " + clan + ".");
+    }
+
     public void extorquer(Commercant victime) {
         parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
         parler(victime.getNom() + ", si tu tiens à la vie donne moi ta bourse !");
